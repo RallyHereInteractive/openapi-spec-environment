983ea34a603d43332be17f986d1556184afa420c
Jon Kenkel <jkenkel@rallyhere.gg>
2023-09-01 12:08:48 -0400

    Keep the minified spec files from each sub-api Reran the generator

--- generate.bash
+++ generate.bash
@@ -33,24 +33,24 @@ set -- "${POSITIONAL_ARGS[@]}" # restore positional parameters
 if [[ ! -z "$SPEC_BASE_URL" ]]; then
     rm -rf schemas
     mkdir -p schemas
-    curl "${SPEC_BASE_URL}/users/openapi.json" -o schemas/users.tmp
-    curl "${SPEC_BASE_URL}/ad/openapi.json" -o schemas/ad.tmp
-    curl "${SPEC_BASE_URL}/settings/openapi.json" -o schemas/settings.tmp
-    curl "${SPEC_BASE_URL}/friends/openapi.json" -o schemas/friends.tmp
-    curl "${SPEC_BASE_URL}/session/openapi.json" -o schemas/session.tmp
-    curl "${SPEC_BASE_URL}/config/openapi.json" -o schemas/config.tmp
-    curl "${SPEC_BASE_URL}/inventory/openapi.json" -o schemas/inventory.tmp
-    curl "${SPEC_BASE_URL}/presence/openapi.json" -o schemas/presence.tmp
-    curl "${SPEC_BASE_URL}/notification/openapi.json" -o schemas/notification.tmp
-    curl "${SPEC_BASE_URL}/rank/openapi.json" -o schemas/rank.tmp
+    curl "${SPEC_BASE_URL}/users/openapi.json" -o schemas/users.min.json
+    curl "${SPEC_BASE_URL}/ad/openapi.json" -o schemas/ad.min.json
+    curl "${SPEC_BASE_URL}/settings/openapi.json" -o schemas/settings.min.json
+    curl "${SPEC_BASE_URL}/friends/openapi.json" -o schemas/friends.min.json
+    curl "${SPEC_BASE_URL}/session/openapi.json" -o schemas/session.min.json
+    curl "${SPEC_BASE_URL}/config/openapi.json" -o schemas/config.min.json
+    curl "${SPEC_BASE_URL}/inventory/openapi.json" -o schemas/inventory.min.json
+    curl "${SPEC_BASE_URL}/presence/openapi.json" -o schemas/presence.min.json
+    curl "${SPEC_BASE_URL}/notification/openapi.json" -o schemas/notification.min.json
+    curl "${SPEC_BASE_URL}/rank/openapi.json" -o schemas/rank.min.json
 
     ########################################
     # Pretty Print the API Specs
-    for filename in schemas/*.tmp; do
-        outfile=$(echo ${filename} | sed -e 's/\.tmp/\.json/')
+    for filename in schemas/*.min.json; do
+        outfile=$(echo ${filename} | sed -e 's/\.min\.json/\.json/')
+        echo "pretty printing ${filename} to ${outfile}"
         echo "$(jq -r . ${filename})" >"${outfile}"
     done
-    rm schemas/*.tmp
 fi
 
 ########################################
