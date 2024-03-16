fcce6b1598110966b1812b90c6a563b5aa7529a1
Jon Kenkel <jkenkel@rallyhere.gg>
2024-03-16 16:23:58 -0400

    Added sanctions-api

--- check_breaking_individual.bash
+++ check_breaking_individual.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-APIS="users ad settings friends session config inventory presence notification rank custom events match"
+APIS="users ad settings friends session config inventory presence notification rank custom events match sanctions"
 
 for api in ${APIS[@]}; do
     echo "Checking ${api} API Spec"
