3602df25823f285e66b8927656dbc994da73582d
Jon Kenkel <jkenkel@rallyhere.gg>
2025-03-25 14:39:59 -0400

    oasdiff moved from Tufin org to oasdiff org

--- install_deps.bash
+++ install_deps.bash
@@ -5,7 +5,7 @@ sudo apt install jq npm curl
 sudo snap install yq
 
 # Install the latest version of oasdiff
-curl -s "https://api.github.com/repos/Tufin/oasdiff/releases/latest" | \
+curl -s "https://api.github.com/repos/oasdiff/oasdiff/releases/latest" | \
     jq -r '.assets[] | select(.name | endswith("_linux_amd64.deb")) | .browser_download_url' | \
     xargs curl -L -o oasdiff_latest_amd64.deb
 sudo dpkg --install oasdiff_latest_amd64.deb
